d0d61408d3885116239fb9ec2f16cec993df77ce
Priyash Laddha <152877779+PriyashLaddha@users.noreply.github.com>
2024-06-18 23:44:39 +0530

    Add files via upload

---
+++ PalindromeNumber.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        // Negative numbers and numbers ending in 0 (except 0 itself) cannot be palindromes
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+
+        int revertedNumber = 0;
+        while (x > revertedNumber) {
+            revertedNumber = revertedNumber * 10 + x % 10;
+            x /= 10;
+        }
+
+        // Check for equality or equality after removing the last digit of the reversed number 
+        return x == revertedNumber || x == revertedNumber / 10;
+    }
+}
